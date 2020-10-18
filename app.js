5bc2df07ad265c5663a9a554ed786883a4959b9c
zi wang <ziwang55@gmail.com>
2020-10-17 22:43:40 -0500

    adding app.js

---
+++ app.js
@@ -0,0 +1,66 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+rock_div = document.getElementById("r");
+paper_div = document.getElementById("p");
+scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win() {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+}
+
+function lose() {
+    
+}
+
+function draw() {
+   
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win();
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+
+    }
+}
+
+function main() {
+    rock_div.addEventListener('click', function () {
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function () {
+        game("p");
+    })
+
+    scissors_div.addEventListener('click', function () {
+        game("s");
+    })
+}
+
+main();
